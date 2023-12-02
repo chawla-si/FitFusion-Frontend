697a51aa8f6bf01142726cc3f683897dcacce42c
Mihir Adelkar <adelkar.m@northeastern.edu>
2023-12-02 15:40:54 -0500

    added image to contact us form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -67,7 +67,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             <form
               target="_blank"
               onSubmit={onSubmit}
-              action="https://formsubmit.co/taxawik915@notedns.com"
+              action="https://formsubmit.co/d9a8ac4bf5da7e8d88cea99477ce51ab "
               method="POST"
             >
               <input
@@ -130,6 +130,25 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                 SUBMIT
               </button>
             </form>
+          </motion.div>
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                className="w-full"
+                alt="Contact Us Page Graphic"
+                src={ContactUsPageGraphic}
+              />
+            </div>
           </motion.div>
         </div>
       </motion.div>
