2c2c5335915009c695f0ca00d136117c79bdfd85
Mihir Adelkar <adelkar.m@northeastern.edu>
2023-12-02 14:55:45 -0500

    added motion to home

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import Sponsor2 from "@/assets/Sponsor2.png";
 import Sponsor3 from "@/assets/Sponsor3.png";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -18,11 +19,24 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* main header and image for the home page*/}
-      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* header */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* headings */}
-          <div>
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
@@ -34,9 +48,19 @@ const Home = ({ setSelectedPage }: Props) => {
               aliquam laudantium excepturi quas tenetur cum dolorem, accusantium
               deserunt? Praesentium, rem!
             </p>
-          </div>
+          </motion.div>
           {/* buttons */}
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Us
             </ActionButton>
@@ -47,12 +71,12 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img alt="Home Page Graphic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,86 +1,119 @@
-import React from 'react'
-import {useState} from 'react'
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import Logo from "@/assets/Logo.png"
-import Link from './Link';
-import { SelectedPage } from '@/shared/types';
-import useMediaQuery from '@/hooks/useMediaQuery';
-import ActionButton from '@/shared/ActionButton';
-
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
-    isTopofPage : boolean
-    selectedPage: SelectedPage
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  isTopofPage: boolean;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = ({isTopofPage, selectedPage,setSelectedPage}: Props) => {
-    const flexBetween= "flex items-center justify-between";
-    const isAboveMediumScreen= useMediaQuery("(min-width: 1060px)")
-    const [isMenuToggeled, setisMenuToggeled] = useState<boolean>(false)
-    const navBarbackground = isTopofPage? "": "bg-primary-100 drop-shadow";
+const Navbar = ({ isTopofPage, selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggeled, setisMenuToggeled] = useState<boolean>(false);
+  const navBarbackground = isTopofPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
-        <div className={`${navBarbackground} ${flexBetween} fixed top-0 z-20 w-full py-6`}> 
+      <div
+        className={`${navBarbackground} ${flexBetween} fixed top-0 z-20 w-full py-6`}
+      >
         {/* keep nav bar at top occupy full width and padding of 6 */}
-            <div className={`${flexBetween} mx-auto w-5/6 `}>
-                {/* keep inner in center and occupy 5/6th of the width */}
+        <div className={`${flexBetween} mx-auto w-5/6 `}>
+          {/* keep inner in center and occupy 5/6th of the width */}
 
-                {/* LEFT SIDE  space between each item is 16*/}
-                <div className={`${flexBetween} w-full gap-16`}>
-                    <img alt='logo' src ={Logo}></img>
-                </div>
+          {/* LEFT SIDE  space between each item is 16*/}
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img alt="logo" src={Logo}></img>
+          </div>
 
-                {/* RIGHT SIDE- all items to the right of logo*/}
-                {isAboveMediumScreen?
-                (<div className={`${flexBetween} w-full`}>
-                    {/* inner left div */}
-                    <div className={`${flexBetween} gap-8 text-sm`}>
-                        <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page='Benefits' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page='Our Classes' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page='Contact Us' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                    </div>
+          {/* RIGHT SIDE- all items to the right of logo*/}
+          {isAboveMediumScreen ? (
+            <div className={`${flexBetween} w-full`}>
+              {/* inner left div */}
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
 
-                    {/* inner right div */}
-                    
-                    <div className={`${flexBetween} gap-8`}>
-                        <p>Sign In</p>
-                        <ActionButton setSelectedPage={setSelectedPage}>Join US</ActionButton>
-                    </div>
-                    
-                </div>):(
-                <button className='rounded-full bg-secondary-500 p-2'
-                onClick={()=>setisMenuToggeled(!isMenuToggeled)}>
-                    <Bars3Icon className='h-6 w-6 text-white'/>
+              {/* inner right div */}
 
-                </button>  
-                    )}
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join US
+                </ActionButton>
+              </div>
             </div>
+          ) : (
+            <button
+              className="rounded-full bg-secondary-500 p-2"
+              onClick={() => setisMenuToggeled(!isMenuToggeled)}
+            >
+              <Bars3Icon className="h-6 w-6 text-white" />
+            </button>
+          )}
         </div>
-        {/* Mobile menu model */}
-        {!isAboveMediumScreen && isMenuToggeled && (
-            <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
-                {/* Close icon */}
-                <div className='flex justify-end p-12'>
-                    <button onClick={()=>setisMenuToggeled(!isMenuToggeled)}>
-                        <XMarkIcon className='h-6 w-6 text-gray-400'/>
-                    </button>
-                </div>
+      </div>
+      {/* Mobile menu model */}
+      {!isAboveMediumScreen && isMenuToggeled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* Close icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setisMenuToggeled(!isMenuToggeled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
 
-                <div className='ml-[33%] flex flex-col gap-10 text-2xl'>
-                        <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page='Benefits' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page='Our Classes' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page='Contact Us' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                    </div>
-
-            </div>
-        )}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
